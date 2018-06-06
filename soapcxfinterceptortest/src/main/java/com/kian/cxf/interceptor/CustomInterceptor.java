96db8976d9113b56538dd7f20a421b12d88561ec
Kian Ting <kian.ting@outlook.com>
2018-06-06 15:27:20 +1200

    adding maven dependancies for the mule app

--- soapcxfinterceptortest/src/main/java/com/kian/cxf/interceptor/CustomInterceptor.java
+++ soapcxfinterceptortest/src/main/java/com/kian/cxf/interceptor/CustomInterceptor.java
@@ -7,15 +7,12 @@ import java.io.InputStream;
 import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.helpers.IOUtils;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
-//import org.mule.runtime.core.internal.message.DefaultMessageBuilder;
-//import org.mule.runtime.api.event.Event; //.api.Event.getCurrentEvent;
 import org.apache.cxf.phase.Phase;
 import org.mule.api.MuleEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.interceptor.StaxInInterceptor;
-import org.apache.cxf.message.Exchange;
 
 public class CustomInterceptor extends AbstractPhaseInterceptor<SoapMessage>{
 	private final Logger log = LoggerFactory.getLogger(CustomInterceptor.class);
@@ -23,14 +20,10 @@ public class CustomInterceptor extends AbstractPhaseInterceptor<SoapMessage>{
 	public CustomInterceptor() {
 		super(Phase.POST_STREAM);
 		getBefore().add(StaxInInterceptor.class.getName());
-		//addAfter(StaxInInterceptor.class.getName());
-		//FIXME move to mule init bean??? 
-		//org.apache.xml.security.Init.init();
 	}
 
 	@Override
 	public void handleMessage(SoapMessage message) throws Fault {
-		  //Exchange exch = message.getExchange();
 		  InputStream is = (InputStream) message.getContent(InputStream.class);
 		  ByteArrayOutputStream os = new ByteArrayOutputStream();
 		  try {
